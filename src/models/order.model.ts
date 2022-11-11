b1b33a9bd167d637bf4a9c5797e83acba58efe25
Matheus Felipe <fpmitmatheus@yahoo.com>
2022-11-11 10:47:53 -0500

    requisito 4

---
+++ src/models/order.model.ts
@@ -0,0 +1,22 @@
+import { Pool } from 'mysql2/promise';
+import IOrder from '../interfaces/order.interface';
+
+export default class OrderModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<IOrder[]> {
+    const result = await this.connection
+      .execute(`SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) AS productsIds 
+      FROM Trybesmith.Orders AS o
+      INNER JOIN Trybesmith.Products AS p
+      ON p.orderId = o.id
+      GROUP BY o.id
+      ORDER BY o.userId;`);
+    const [rows] = result;
+    return rows as IOrder[];
+  }
+}
